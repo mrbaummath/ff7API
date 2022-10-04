99d6d9b5a9d1a2a8b1632af7639c6b07d3b9e4f8
mrbaummath <baum1286@gmail.com>
2022-10-03 21:39:35 -0400

    landing page, seeding, and index routes complete

--- server.js
+++ server.js
@@ -47,7 +47,6 @@ const materiasSchema = new Schema({
 //create model
 const Materia = model("Materia", materiasSchema)
 module.exports = Materia
-console.log(materiasSchema, Materia)
 
 //application object
 const app = express()
@@ -57,6 +56,44 @@ app.use(morgan("tiny"))
 app.use(express.urlencoded({ extended: true })) 
 app.use(express.static("public")) 
 app.use(express.json())
+
+
+
+//Routes
+//seed
+app.get("/materias/seed", (req, res) => {
+    //starting materia for testing purposes
+    const startMaterias = [
+        {name: "Fire", color: "green", maxAP: 55000, common: true},
+        {name: "Restore", color: "green", maxAP: 85000, common: true},
+        {name: "Knights of the Round", color: "red", maxAP: 1000000, common: false},
+        {name: "All", color: "blue", maxAP: 60000, common: true}
+    ]
+    //delete all materias
+    Materia.deleteMany({}).then((materias) => {
+        //seed
+        Materia.create(startMaterias)
+            .then((Materias) => {
+                res.json(Materias)
+            })
+    })
+})
+
+//landing
+app.get("/", (req, res) => {
+    res.send("Welcome to my Materia API")
+})
+
+//index 
+app.get("/materias", (req, res)=> {
+    Materia.find({})
+        .then((materias) => {
+            res.json({ materias: materias})
+        })
+        .catch((error) => {
+            res.json({ error })
+        })
+})
 
 //Server listener
 const PORT = process.env.PORT
