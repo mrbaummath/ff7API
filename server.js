e1d8edbd204df987c078dfad934504c71c1f27c6
mrbaummath <baum1286@gmail.com>
2022-10-04 17:11:13 -0400

    all initial routes working

--- server.js
+++ server.js
@@ -95,6 +95,51 @@ app.get("/materias", (req, res)=> {
         })
 })
 
+//create
+app.post("/materias", (req, res) => {
+    Materia.create(req.body)
+        .then(materia => {
+            res.json({materia: materia.toObject()})
+        })
+        .catch(err => console.log(err))
+})
+
+//update 
+app.put("/materias/:id", (req,res) => {
+    const id = req.params.id
+    //we'll use a single mongoose model method for now but this will need to be updated. FindByIdandUpdate needs and id, a req.body, and whether the info is new
+    Materia.findByIdAndUpdate(id,req.body, {new: true})
+        .then(materia => {
+            console.log('updated', materia)
+            res.sendStatus(204)
+        })
+        .catch(err => console.log(err))
+
+})
+
+//delete 
+app.delete('/materias/:id', (req,res) => {
+    const id = req.params.id
+    Materia.findByIdAndRemove(id)
+    .then(materia => {
+        res.sendStatus(204)
+    })
+    .catch(err => console.log(err))
+})
+
+//show
+app.get('/materias/:id', (req,res) => {
+    const id = req.params.id
+    Materia.findById(id)
+        .then(materia => {
+            console.log(materia)
+            res.json({materia: materia})
+        })
+        .catch(err => console.log(err))
+
+})
+
+
 //Server listener
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
