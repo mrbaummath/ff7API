4df141a3004b1c5886bcdfa2bcba5c0422a6a097
mrbaummath <baum1286@gmail.com>
2022-10-04 23:00:10 -0400

    user routes login and logout completed

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -9,6 +9,7 @@ const router = express.Router()
 
 //Routes
 
+//signup
 router.post('/signup', async (req, res) => {
     req.body.password = await bcrypt.hash(
         req.body.password,
@@ -20,6 +21,40 @@ router.post('/signup', async (req, res) => {
             res.status(201).json({ username: user.username })
         })
         .catch(err => console.log(err))
+})
+
+//login 
+router.post('/login', (req, res) => {
+    const { username, password } = req.body
+    User.findOne({ username })
+    .then(async (user) => {
+        if (user) {
+            const result = await bcrypt.compare(password, user.password)
+            if (result) {
+                req.session.username = username
+                req.session.userId = user.id
+                res.status(201).json({ user: user.toObject() })
+            } else {
+                res.json({ error: 'username or password incorrect' })
+            }
+
+        } else {
+            res.json({ error: 'user dne' })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.json(err)
+    })
+})
+
+//logout
+router.delete('/logout', (req,res) => {
+    req.session.destroy(err => {
+        console.log('logged out', req.session)
+        console.log('error on logout?', err)
+        res.sendStatus(204)
+    })
 })
 
 module.exports = router
