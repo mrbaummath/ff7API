676b7732c057f1cd09918be48b869c2485d4fef3
mrbaummath <baum1286@gmail.com>
2022-10-17 11:07:58 -0400

    deployed url

--- models/connection.js
+++ models/connection.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose')
 
 //database connection
 const DATABASE_URL = process.env.DATABASE_URL
+
+const DEPLOYED_URL = process.env.DEPLOYED_URL
+
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
 
-mongoose.connect(DATABASE_URL, CONFIG)
+mongoose.connect(DEPLOYED_URL, CONFIG)
 
 mongoose.connection
     .on("open", () => console.log("Connected to db"))
